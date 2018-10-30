d3374cfe90f4986e21fe3fede1c5f3e02d429906
Angela Li <angelali921@gmail.com>
2018-10-30 14:36:23 -0500

    Add README, clean up script

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# chicago-housing
-For exploring Chicago affordable housing data
+
+About this repository
+---------------------
+
+This repository contains the script produced during the R Spatial Workshop on October 30, 2018. Click on [`chicago-housing-explore.R`](https://github.com/angela-li/chicago-housing/blob/master/chicago-housing-explore.R) above to see the code produced in the workshop.
+
+We'll be building out this script over multiple workshops. Expect more maps soon!
+
+Additional resources
+--------------------
+
+-   To get a preview of what we'll be doing, check out Luc's tutorial for [Spatial Data Handling](https://spatialanalysis.github.io/lab_tutorials/1_R_Spatial_Data_Handling.html#community-area-population-data). We're working with different data, but the concepts are similar.
+
+-   To get more familiar with dplyr, there's no better resource than [R for Data Science, chapter 5](https://r4ds.had.co.nz/transform.html). This book in general is *fantastic* and I recommend you give it a look.
+
+-   To download more Chicago-related data, go to the [Chicago Data Portal](https://data.cityofchicago.org)here.
--- chicago-housing-explore.R
+++ chicago-housing-explore.R
@@ -1,23 +1,30 @@
+# This script loads and explores affordable housing data from the City of Chicago data portal
+# Link: https://data.cityofchicago.org/Community-Economic-Development/Affordable-Rental-Housing-Developments/s6ha-ppgi
+
+
+# Load packages -----------------------------------------------------------
 library(RSocrata)
 library(dplyr)
 
-# Import data
+
+# Import data -------------------------------------------------------------
 socrata_url <- "https://data.cityofchicago.org/resource/uahe-iimk.csv"
-housing <- read.socrata(socrata_url) %>% 
+housing_tbl <- read.socrata(socrata_url) %>% 
   as_tibble()
+# Ctrl-Shift-M to produce "pipe" symbol
 
-#Ctrl Shift M
 
-# Explore data
-class(housing)
-head(housing)
-View(housing)
-str(housing)
+# Explore data ------------------------------------------------------------
+class(housing_tbl)
+head(housing_tbl)
+View(housing_tbl)
+str(housing_tbl)
 
-# Ctrl-Enter to run
+# Ctrl-Enter to run one line
 # press Tab to autocomplete!
 
-# How many properties in each area have affordable housing in them?
+
+# How many properties in each area have affordable housing in them? -------
 properties_per_area <- housing_tbl %>% 
   group_by(community_area_number) %>% 
   tally()
@@ -33,8 +40,8 @@ left_join(properties_per_area,
   select(community_area, community_area_number, properties = n) %>% 
   arrange(desc(properties))
 
-# Challenge: find neighborhoods with most housing UNITS
 
+# Challenge: find neighborhoods with most housing UNITS -------------------
 housing_tbl %>% 
   group_by(community_area_number) %>% 
   summarize(total_units = sum(units))
