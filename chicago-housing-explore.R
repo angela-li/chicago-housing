116c8211827db801c4f41e2df8ff0b847ecf4e66
Angela Li <angelali921@gmail.com>
2018-11-06 12:45:01 -0600

    Add code up to this point

--- chicago-housing-explore.R
+++ chicago-housing-explore.R
@@ -5,7 +5,7 @@
 # Load packages -----------------------------------------------------------
 library(RSocrata)
 library(dplyr)
-
+library(ggplot2)
 
 # Import data -------------------------------------------------------------
 socrata_url <- "https://data.cityofchicago.org/resource/uahe-iimk.csv"
@@ -42,6 +42,24 @@ left_join(properties_per_area,
 
 
 # Challenge: find neighborhoods with most housing UNITS -------------------
-housing_tbl %>% 
+
+units_per_area <- housing_tbl %>% 
   group_by(community_area_number) %>% 
-  summarize(total_units = sum(units))
+  summarize(total_units = sum(units)) %>% 
+  left_join(units_per_area, 
+          areas_key, 
+          by = "community_area_number") %>% 
+  select(community_area, community_area_number, total_units) %>% 
+  arrange(desc(total_units))
+
+# Ctrl-Shift R to insert a new section
+
+# Visualize data ----------------------------------------------------------
+ggplot(units_per_area, aes(x = total_units)) +
+  geom_histogram(binwidth = 100) +
+  labs(x = "Total Units By Community Area", y = "Count")
+
+# make a histogram for the number of properties per area
+
+
+names(units_per_area)
